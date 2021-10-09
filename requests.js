60d43aa2ec024f333060730de9dbdd3190c47211
Sasha Petrov <brat77@tut.by>
2021-10-09 14:42:29 +0300

    refactor task

--- requests.js
+++ requests.js
@@ -11,73 +11,74 @@ const requests = {
    * если всё выполнилось хорошо, тогда надо вызвать cb(null, response) если была ошибка - cb(error)
    * */
   httpGet(url, params, options, cb) {
-      const fullUrl = url + '?'
-           + objectToQuery(params);
-          
-      let x = new XMLHttpRequest();
-          x.open("GET", fullUrl, options);
-          x.send();
-          x.onload = () => {
-            let data = x.response;
-            cb(null, data);
-           };
-          x.onerror = () => {
-            cb(error);
-          };                  
-    },
+    const fullUrl = url + "?" + objectToQuery(params);
+
+    let x = new XMLHttpRequest();
+    x.open("GET", fullUrl, options);
+    x.onload = () => {
+      let data = x.response;
+      cb(null, data);
+    };
+    x.onerror = (error) => {
+      cb(error);
+    };
+
+    x.send();
+  },
 
   /**
    * смотреть описание httpGet
    * метод будет делать почти то же самое только данные будут отправляться через POST
    */
   httpPost(url, urlParams, params, options, headers, timeout, sync, cb) {
-    const fullUrl = url + '?'
-           + objectToQuery(urlParams);
+    const fullUrl = url + "?" + objectToQuery(urlParams);
     const body = JSON.stringify(params);
-          
-      let x = new XMLHttpRequest();
-          x.open("POST", fullUrl, !sync, options);
-          x.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-//set headers // headers = {...}
-          Object.entries(headers).forEach(([key, value]) => {
-            x.setRequestHeader(`'${key}', '${value}'`);
-          })
 
-//timeout
-          x.timeout = timeout;
-          x.ontimeout = () => {
-            console.log('Загрузка прервана - превышено время ожидания ответа сервера');
-          };
+    const x = new XMLHttpRequest();
+    x.open("POST", fullUrl, !sync, options);
+    x.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    //set headers // headers = {...}
+    Object.entries(headers).forEach(([key, value]) => {
+      x.setRequestHeader(`'${key}', '${value}'`);
+    });
 
-          x.send(body);
-          x.onload = () => {
-            let data = x.response;
-            cb(null, data);
-           };
-          x.onerror = () => {
-            cb(error);
-          };                  
-   },
+    //timeout
+    x.timeout = timeout;
+    x.ontimeout = () => {
+      cb(new Error("timeout"));
+    };
+
+    x.onload = () => {
+      const data = x.response;
+      cb(null, data);
+    };
+    x.onerror = () => {
+      cb(error);
+    };
+
+    x.send(body);
+  },
 
   /**+
    * ф-ция на вход получает объект {key1: value1, key2: value2,.... } и возваращет строку в виде key1=value1&key2=value2....
    */
   objectToQuery(obj) {
-    return Object.entries(obj).map(([key, value]) => {
-      return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
-    }).join('&');
-
-   },
+    return Object.entries(obj)
+      .map(([key, value]) => {
+        return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+      })
+      .join("&");
+  },
   /**
    * ф-ция на вход получает строку в виде key1=value1&key2=value2.... и возваращет объект {key1: value1, key2: value2,.... }
    */
   queryToObject(query) {
-    const arrParam = query.split('&');
+    const arrParam = query.split("&");
     return arrParam.reduce((acc, current) => {
-      const [key, value] = current.split('=');
+      const [key, value] = current.split("=");
       acc[decodeURIComponent(key)] = decodeURIComponent(value);
-      console.log(typeof(current));
+      console.log(typeof current);
       return acc;
-    },{} ) 
-  }
+    }, {});
+  },
 };
